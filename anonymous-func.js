53233a76470deee6ba417ca14238d7c074da6956
Aiman-xo <aimanrasack@gmail.com>
2025-07-14 16:38:30 +0530

    Add files via upload

---
+++ anonymous-func.js
@@ -0,0 +1,26 @@
+//GPT about anonymous functions questi0ns
+const anony = function (num) {
+    return num ** 3;
+}
+console.log(anony(3));
+
+//2
+
+setTimeout(function () {
+    console.log("timer done");
+}, 2000);
+
+//3
+
+const arr = ["Ali", "Sara", "Vikram"];
+
+arr.forEach((val) => {
+    console.log(val);
+})
+
+//4
+
+const square = (num) => {
+    return num ** 2;
+}
+console.log(square(2));
